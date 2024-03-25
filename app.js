54ee6ac9ea60fc8704f7521b1bb3e84eedba85d2
spezdmtest <spezd.m.@gmail.com>
2024-03-25 16:52:05 +0200

    add function generateRandomColor()

---
+++ app.js
@@ -0,0 +1,16 @@
+const cols = document.querySelectorAll(".col");
+
+function generateRandomColor() {
+    //RGB
+    //FF0000
+    //00FF00
+    //0000FF
+  
+    const hexCodes = "0123456789ABCDEF";
+    let color = "";
+    for (let i = 0; i < 6; i++) {
+      color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+    }
+  
+    return "#" + color;
+  }
