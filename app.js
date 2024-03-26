5d25f672bdcbf707253c45f2aade123f9daa19be
spezdmtest <spezd.m.@gmail.com>
2024-03-26 15:46:19 +0200

    luminance

--- app.js
+++ app.js
@@ -19,9 +19,17 @@ function setRendomColors() {
     cols.forEach((col) => {
       const text = col.querySelector("h2");
       const color = chroma.random();
+      
       text.textContent = color;
       col.style.background = color;
+
+      setTextColor(text, color);
     });
   }
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? "black" : "white";
+}  
 
   setRendomColors();
