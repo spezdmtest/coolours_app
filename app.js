80eea89534636ea24b49f66133f8dd5b4c8ad828
spezdmtest <spezd.m.@gmail.com>
2024-03-26 15:46:55 +0200

    add luminance

--- app.js
+++ app.js
@@ -16,20 +16,20 @@ function generateRandomColor() {
 }
 
 function setRendomColors() {
-    cols.forEach((col) => {
-      const text = col.querySelector("h2");
-      const color = chroma.random();
-      
-      text.textContent = color;
-      col.style.background = color;
+  cols.forEach((col) => {
+    const text = col.querySelector("h2");
+    const color = chroma.random();
 
-      setTextColor(text, color);
-    });
-  }
+    text.textContent = color;
+    col.style.background = color;
+
+    setTextColor(text, color);
+  });
+}
 
 function setTextColor(text, color) {
   const luminance = chroma(color).luminance();
   text.style.color = luminance > 0.5 ? "black" : "white";
-}  
+}
 
-  setRendomColors();
+setRendomColors();
