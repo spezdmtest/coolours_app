0e633d38e7f5a52697034dd34cef1c86c8efaea0
spezdmtest <spezd.m.@gmail.com>
2024-03-27 17:25:37 +0200

    add hash color

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ function generateRandomColor() {
 
 function copyToClickboard(text) {
   return navigator.clipboard.writeText(text);
-
 }
 
 function setRendomColors() {
+  const colors = [];
   cols.forEach((col) => {
     const isLocked = col.querySelector("i").classList.contains("fa-lock");
     const text = col.querySelector("h2");
@@ -53,8 +53,11 @@ function setRendomColors() {
     const color = chroma.random();
 
     if (isLocked) {
+      colors.push(text.textContent);
       return;
     }
+
+    colors.push(color);
 
     text.textContent = color;
     col.style.background = color;
@@ -62,11 +65,22 @@ function setRendomColors() {
     setTextColor(text, color);
     setTextColor(button, color);
   });
+
+  updateColorsHash(colors);
 }
 
 function setTextColor(text, color) {
   const luminance = chroma(color).luminance();
   text.style.color = luminance > 0.5 ? "black" : "white";
+}
+
+function updateColorsHash(colors = []) {
+  document.location.hash = colors
+    .map((col) => {
+      console.log(col);
+      return col.toString().substring(1);
+    })
+    .join("-");
 }
 
 setRendomColors();
